9a2641fccb864665880d58d15301bc48f4ec779e
Alex <Alex@Samurai>
2015-11-08 17:53:04 -0600

    Removed Assignment 4 folder to avoid confusion, also further updated manage serviceRecordGUI

--- src/ManageServiceRecord.java
+++ src/ManageServiceRecord.java
@@ -14,6 +14,7 @@ import javax.swing.border.EmptyBorder;
 import java.awt.SystemColor;
 import javax.swing.UIManager;
 import java.awt.Font;
+import java.awt.Color;
 
 public class ManageServiceRecord extends JDialog implements ActionListener{
 	private DefaultListModel results;
@@ -63,73 +64,73 @@ public class ManageServiceRecord extends JDialog implements ActionListener{
 		
 		JLabel ServiceRecordsLabel = new JLabel("Service Records");
 		ServiceRecordsLabel.setFont(new Font("Myriad Pro", Font.BOLD, 19));
-		ServiceRecordsLabel.setBounds(227, 0, 226, 20);
+		ServiceRecordsLabel.setBounds(227, 11, 226, 20);
 		getContentPane().add(ServiceRecordsLabel);
 		
 		DateTextField = new JTextField();
-		DateTextField.setBounds(95, 31, 142, 20);
+		DateTextField.setBounds(106, 42, 142, 20);
 		getContentPane().add(DateTextField);
 		DateTextField.setColumns(10);
 		
 		CommentsTextField = new JTextField();
-		CommentsTextField.setBounds(363, 31, 206, 69);
+		CommentsTextField.setBounds(363, 42, 206, 69);
 		getContentPane().add(CommentsTextField);
 		CommentsTextField.setColumns(10);
 		
 		MemberNumberTextField = new JTextField();
-		MemberNumberTextField.setBounds(95, 162, 142, 20);
+		MemberNumberTextField.setBounds(106, 203, 142, 20);
 		getContentPane().add(MemberNumberTextField);
 		MemberNumberTextField.setColumns(10);
 		
 		ServiceCodeTextField = new JTextField();
-		ServiceCodeTextField.setBounds(363, 117, 206, 20);
+		ServiceCodeTextField.setBounds(363, 168, 206, 20);
 		getContentPane().add(ServiceCodeTextField);
 		ServiceCodeTextField.setColumns(10);
 		
 		TimeTextField = new JTextField();
-		TimeTextField.setBounds(95, 73, 142, 20);
+		TimeTextField.setBounds(106, 91, 142, 20);
 		getContentPane().add(TimeTextField);
 		TimeTextField.setColumns(10);
 		
 		ProviderNumberTextField = new JTextField();
-		ProviderNumberTextField.setBounds(95, 117, 142, 20);
+		ProviderNumberTextField.setBounds(106, 145, 142, 20);
 		getContentPane().add(ProviderNumberTextField);
 		ProviderNumberTextField.setColumns(10);
 		
 		JLabel DateLabel = new JLabel("Date");
 		DateLabel.setFont(new Font("Myriad Pro", Font.PLAIN, 11));
-		DateLabel.setForeground(UIManager.getColor("activeCaption"));
-		DateLabel.setBounds(10, 34, 75, 14);
+		DateLabel.setForeground(new Color(0, 100, 0));
+		DateLabel.setBounds(21, 46, 75, 14);
 		getContentPane().add(DateLabel);
 		
 		JLabel TimeLabel = new JLabel("Time");
-		TimeLabel.setForeground(UIManager.getColor("activeCaption"));
+		TimeLabel.setForeground(new Color(0, 128, 0));
 		TimeLabel.setFont(new Font("Myriad Pro", Font.PLAIN, 11));
-		TimeLabel.setBounds(10, 76, 75, 14);
+		TimeLabel.setBounds(21, 97, 75, 14);
 		getContentPane().add(TimeLabel);
 		
 		JLabel ProviderNumberLabel = new JLabel("Provider Number");
-		ProviderNumberLabel.setForeground(UIManager.getColor("activeCaption"));
+		ProviderNumberLabel.setForeground(new Color(0, 128, 0));
 		ProviderNumberLabel.setFont(new Font("Myriad Pro", Font.PLAIN, 11));
-		ProviderNumberLabel.setBounds(10, 120, 111, 14);
+		ProviderNumberLabel.setBounds(21, 149, 111, 14);
 		getContentPane().add(ProviderNumberLabel);
 		
 		JLabel CommentsLabel = new JLabel("Comments");
 		CommentsLabel.setFont(new Font("Myriad Pro", Font.PLAIN, 11));
-		CommentsLabel.setForeground(UIManager.getColor("activeCaption"));
-		CommentsLabel.setBounds(284, 34, 68, 14);
+		CommentsLabel.setForeground(new Color(0, 128, 0));
+		CommentsLabel.setBounds(284, 70, 68, 14);
 		getContentPane().add(CommentsLabel);
 		
 		JLabel MemberLabel = new JLabel("Member Number");
 		MemberLabel.setFont(new Font("Myriad Pro", Font.PLAIN, 11));
-		MemberLabel.setForeground(UIManager.getColor("activeCaption"));
-		MemberLabel.setBounds(10, 165, 111, 14);
+		MemberLabel.setForeground(new Color(0, 128, 0));
+		MemberLabel.setBounds(21, 207, 111, 14);
 		getContentPane().add(MemberLabel);
 		
 		JLabel ServiceCodeLabel = new JLabel("Service Code");
-		ServiceCodeLabel.setForeground(UIManager.getColor("activeCaption"));
+		ServiceCodeLabel.setForeground(new Color(0, 128, 0));
 		ServiceCodeLabel.setFont(new Font("Myriad Pro", Font.PLAIN, 11));
-		ServiceCodeLabel.setBounds(284, 120, 111, 14);
+		ServiceCodeLabel.setBounds(284, 172, 111, 14);
 		getContentPane().add(ServiceCodeLabel);
 		
 	
