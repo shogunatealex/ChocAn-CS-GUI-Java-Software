2af569739ac2c9a6d1bd6e72d5f85957d03e9dbf
eadwyer <eadwyer@crimson.ua.edu>
2015-11-16 14:31:58 -0600

    actual javadoc

--- src/ManageProviderRecord.java
+++ src/ManageProviderRecord.java
@@ -16,16 +16,14 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 
 /**
- * @author Alex Anderson
- * Allows for entering/ editing information in
- * provider record.
+ * @author Alex Anderson Allows for entering/ editing information in provider
+ *         record.
  *
  */
-public class ManageProviderRecord extends ManageRecordGUI implements ActionListener{
+public class ManageProviderRecord extends ManageRecordGUI implements ActionListener {
 
 	private JTextField ProvNumTextField;
 
-	
 	/**
 	 * Launch the application.
 	 */
@@ -41,9 +39,12 @@ public class ManageProviderRecord extends ManageRecordGUI implements ActionListe
 
 	/**
 	 * Create the dialog.
+	 * 
+	 * @param PR
+	 *            providerRecord
 	 */
-	
-	public ManageProviderRecord(ProviderRecord PR){
+
+	public ManageProviderRecord(ProviderRecord PR) {
 		buildLocalPane();
 		ProvNumTextField.setText(String.format("%09d", PR.getProviderNumber()));
 		NameTextField.setText(PR.getName());
@@ -53,60 +54,59 @@ public class ManageProviderRecord extends ManageRecordGUI implements ActionListe
 		AddressTextField.setText(PR.getAddress());
 		setVisible(true);
 	}
-	
+
 	public ManageProviderRecord() {
 		buildLocalPane();
 		setVisible(true);
-		
+
 	}
-	/*
+
+	/**
 	 * Display components and fields of provider record.
 	 */
-	private void buildLocalPane(){
+	private void buildLocalPane() {
 		super.buildPane();
 		setTitle("Manage Provider Record Editor");
 
-		
 		JLabel ProviderRecordsLabel = new JLabel("Provider Records");
 		ProviderRecordsLabel.setFont(new Font("Myriad Pro", Font.BOLD, 19));
 		ProviderRecordsLabel.setBounds(272, 11, 271, 20);
 		window.add(ProviderRecordsLabel);
-		
-		
+
 		ProvNumTextField = new JTextField();
 		ProvNumTextField.setBounds(159, 105, 86, 20);
 		window.add(ProvNumTextField);
 		ProvNumTextField.setColumns(10);
-		
 
-		
 		JLabel ProviderNumberLabel = new JLabel("Provider Number");
 		ProviderNumberLabel.setForeground(new Color(0, 100, 0));
 		ProviderNumberLabel.setFont(new Font("Myriad Pro", Font.PLAIN, 11));
 		ProviderNumberLabel.setBounds(43, 108, 111, 14);
 		window.add(ProviderNumberLabel);
-		
+
 	}
-	/*
-	 * @return provider number
+
+	/**
+	 * @return providerNumber
 	 */
-	public int getProviderNumber(){ 
+	public int getProviderNumber() {
 		return Integer.parseInt(ProvNumTextField.getText());
 	}
-/*
- * (non-Javadoc)
- * @see ManageRecordGUI#actionPerformed(java.awt.event.ActionEvent)
- */
+
+	/**
+	 * @see ManageRecordGUI#actionPerformed(java.awt.event.ActionEvent) If back
+	 *      selected, do not update provider record. If Ok selected, update
+	 *      provider record.
+	 */
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == BackButton){
+		if (e.getSource() == BackButton) {
 			Cancel = true;
 			setVisible(false);
-			
-		}
-		else if (e.getSource() == OkButton){
+
+		} else if (e.getSource() == OkButton) {
 			Cancel = false;
 			setVisible(false);
 		}
-		
+
 	}
 }
