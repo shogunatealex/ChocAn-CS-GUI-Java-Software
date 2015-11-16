c67287b99df0ec31f6d6064967b39e53d36af796
Alex Anderson <alexanderson234@gmail.com>
2015-11-16 15:25:48 -0600

    Added manually

---
+++ src/AccountingController.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+//David
+
+public class AccountingController {
+	public AccountingController() {
+    	
+    }
+    public void createReports() {
+    	System.out.println("Main accounting procedure not yet implemented");
+    }
+    public void createMemberReport(int memberNumber) {
+    	MemberRecord Record = FirstGUI.MRC.getSpecificRecordByMemberNumber(memberNumber);
+    	System.out.println(Record.getName());
+    }
+    public void createProviderReport(int providerNumber) {
+    	ProviderRecord Record = FirstGUI.PRC.getSpecificRecordByProviderNumber(providerNumber);
+    	System.out.println(Record.getName());
+    }
+    public void createEFTReport() {
+    	System.out.println("EFT reports not yet implemented");
+    }
+    public void createSummaryReport() {
+    	System.out.println("Summary reports not yet implemented");
+    }
+    
+}
+
