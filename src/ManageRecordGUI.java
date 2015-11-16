27357da87dbb6b6ec4d42f2969c80fc7e3e88394
eadwyer <eadwyer@crimson.ua.edu>
2015-11-16 14:33:24 -0600

    actual javadoc

--- src/ManageRecordGUI.java
+++ src/ManageRecordGUI.java
@@ -19,9 +19,8 @@ import javax.swing.JTextArea;
 import java.awt.Color;
 
 /**
- * @author Alex Anderson
- * Allow for adding, editing, and deleting provider records.
- * Displays provider records.
+ * @author Alex Anderson Allow for adding, editing, and deleting provider
+ *         records. Displays provider records. Abstract class
  *
  */
 public abstract class ManageRecordGUI extends JDialog implements ActionListener {
@@ -36,6 +35,7 @@ public abstract class ManageRecordGUI extends JDialog implements ActionListener 
 	protected JDialog Owner;
 	protected Container window;
 	protected boolean Cancel = true;
+
 	/**
 	 * Used primarily for testing/ independent launching
 	 */
@@ -49,13 +49,14 @@ public abstract class ManageRecordGUI extends JDialog implements ActionListener 
 		}
 	}
 
-/*
- * @return canceled if canceled.	
- */
-	public boolean isCanceled(){
+	/**
+	 * @return canceled if canceled.
+	 */
+	public boolean isCanceled() {
 		return Cancel;
 	}
-	/*
+
+	/**
 	 * Display contents.
 	 */
 	public void buildPane() {
@@ -65,125 +66,119 @@ public abstract class ManageRecordGUI extends JDialog implements ActionListener 
 		setBounds(100, 100, 450, 300);
 		window.setLayout(null);
 		setModal(true);
-		
+
 		OkButton = new JButton("Ok");
 		OkButton.addActionListener(this);
 		OkButton.setBounds(429, 257, 114, 33);
 		window.add(OkButton);
-		
+
 		BackButton = new JButton("Back");
 		BackButton.setBounds(560, 257, 114, 33);
 		BackButton.addActionListener(this);
 		window.add(BackButton);
-		
+
 		NameTextField = new JTextField("");
 		NameTextField.setBounds(159, 62, 86, 20);
 		getContentPane().add(NameTextField);
 		NameTextField.setColumns(10);
-		
+
 		CityTextField = new JTextField("");
 		CityTextField.setBounds(457, 135, 142, 20);
 		getContentPane().add(CityTextField);
 		CityTextField.setColumns(10);
-		
+
 		StateTextField = new JTextField("");
 		StateTextField.setBounds(457, 179, 62, 20);
 		getContentPane().add(StateTextField);
 		StateTextField.setColumns(10);
-		
+
 		ZipCodeTextField = new JTextField("");
 		ZipCodeTextField.setBounds(159, 148, 86, 20);
 		getContentPane().add(ZipCodeTextField);
 		ZipCodeTextField.setColumns(10);
-		
+
 		JLabel NameLabel = new JLabel("Name");
 		NameLabel.setFont(new Font("Myriad Pro", Font.PLAIN, 11));
 		NameLabel.setForeground(new Color(0, 100, 0));
 		NameLabel.setBounds(45, 65, 104, 14);
 		getContentPane().add(NameLabel);
-		
+
 		JLabel ZipcodeLabel = new JLabel("ZipCode");
 		ZipcodeLabel.setFont(new Font("Myriad Pro", Font.PLAIN, 11));
 		ZipcodeLabel.setForeground(new Color(0, 100, 0));
 		ZipcodeLabel.setBounds(45, 151, 104, 14);
 		getContentPane().add(ZipcodeLabel);
-		
+
 		JLabel lblNewLabel_3 = new JLabel("Address");
 		lblNewLabel_3.setFont(new Font("Myriad Pro", Font.PLAIN, 11));
 		lblNewLabel_3.setForeground(new Color(0, 100, 0));
 		lblNewLabel_3.setBounds(331, 65, 116, 14);
 		getContentPane().add(lblNewLabel_3);
-		
+
 		JLabel lblNewLabel_4 = new JLabel("City");
 		lblNewLabel_4.setFont(new Font("Myriad Pro", Font.PLAIN, 11));
 		lblNewLabel_4.setForeground(new Color(0, 100, 0));
 		lblNewLabel_4.setBounds(333, 138, 114, 14);
 		getContentPane().add(lblNewLabel_4);
-		
+
 		JLabel lblNewLabel_5 = new JLabel("State");
 		lblNewLabel_5.setFont(new Font("Myriad Pro", Font.PLAIN, 11));
 		lblNewLabel_5.setForeground(new Color(0, 100, 0));
 		lblNewLabel_5.setBounds(331, 182, 116, 14);
 		getContentPane().add(lblNewLabel_5);
-		
+
 		AddressTextField = new JTextArea();
 		AddressTextField.setBackground(Color.WHITE);
 		AddressTextField.setBounds(457, 42, 149, 69);
 		AddressTextField.setLineWrap(true);
 		getContentPane().add(AddressTextField);
-		
-	
-		
-		
-	    setSize( 700, 340 );
-	    setLocation( 100, 100 );
+
+		setSize(700, 340);
+		setLocation(100, 100);
 	}
-/*
- * (non-Javadoc)
- * @see java.awt.Component#getName()
- * @return name
- */
-	public String getName(){
+
+	/**
+	 * @see java.awt.Component#getName()
+	 * @return name
+	 */
+	public String getName() {
 		return NameTextField.getText();
 	}
-	/*
-	 * (non-Javadoc)
+
+	/**
 	 * @see java.awt.Component#getName()
 	 * @return city
 	 */
-	public String getCity(){
+	public String getCity() {
 		return CityTextField.getText();
 	}
-	/*
-	 * (non-Javadoc)
+
+	/**
 	 * @see java.awt.Component#getName()
 	 * @return state
 	 */
-	public String getState(){
+	public String getState() {
 		return StateTextField.getText();
 	}
-	/*
-	 * (non-Javadoc)
+
+	/**
 	 * @see java.awt.Component#getName()
 	 * @return zipCode
 	 */
-	public int getZipCode(){
+	public int getZipCode() {
 		return Integer.parseInt(ZipCodeTextField.getText());
 	}
-	/*
-	 * (non-Javadoc)
+
+	/**
 	 * @see java.awt.Component#getName()
 	 * @return address
 	 */
-	public String getAddress(){
+	public String getAddress() {
 		return AddressTextField.getText();
 	}
-	
-	
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
-		
 	}
 }
