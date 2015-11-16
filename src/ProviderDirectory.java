5746c4301f5c1ce93a509dcbacd0a8b06abbbb4a
eadwyer <eadwyer@crimson.ua.edu>
2015-11-16 14:52:12 -0600

    actual javadoc

--- src/ProviderDirectory.java
+++ src/ProviderDirectory.java
@@ -19,41 +19,53 @@ public class ProviderDirectory implements Serializable {
 		this.cost = costs;
 	}
 
-	/*
+	/**
 	 * @return cost
 	 */
 	public double get_Cost() {
 		return this.cost;
 	}
 
+	/**
+	 * 
+	 * @param newCost
+	 */
 	public void setCost(double newCost) {
 		this.cost = newCost;
 	}
 
-	/*
+	/**
 	 * @return serviceName
 	 */
 	public String get_Service() {
 		return this.Service;
 	}
 
+	/**
+	 * 
+	 * @param newService
+	 */
 	public void set_Service(String newService) {
 		this.Service = newService;
 	}
 
-	/*
+	/**
 	 * @return serviceCode
 	 */
 	public int get_sNumber() {
 		return this.sNumber;
 	}
 
+	/**
+	 * 
+	 * @param num
+	 *            serviceCode
+	 */
 	public void set_sNumber(int num) {
 		this.sNumber = num;
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
 	 * 
 	 * @see java.lang.Object#toString()
 	 * 
