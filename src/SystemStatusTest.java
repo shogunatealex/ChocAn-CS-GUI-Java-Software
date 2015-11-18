96061534a1fa91663fd3ec25117868354b30c0f2
eadwyer <eadwyer@crimson.ua.edu>
2015-11-18 13:42:28 -0600

    JUnit test for SystemStatus

---
+++ src/SystemStatusTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+
+public class SystemStatusTest {
+
+	SystemStatus newSystem = new SystemStatus();
+
+	@Before
+	public void setUp() {
+		SystemStatus newSystem = new SystemStatus();
+	}
+
+	@Test
+	public void testGetStatus() {
+		assertTrue("Get Status", newSystem.checkMode()); // system is in
+															// interactive
+	}
+
+	@Test
+	public void testToggleStatus() {
+		newSystem.toggleInteractiveMode();
+		assertFalse("Get Status", newSystem.checkMode()); // system is not
+															// interactive
+	}
+}
