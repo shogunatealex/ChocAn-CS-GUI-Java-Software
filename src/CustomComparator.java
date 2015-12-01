94622af0a2e7ec7e4f8eb4afa2ed3341a68c0a2d
Trevor Gentner <Trevor Gentner@Trevor>
2015-11-30 21:29:58 -0600

    got date sort to sort by year first

--- src/CustomComparator.java
+++ src/CustomComparator.java
@@ -9,6 +9,19 @@ import java.util.Comparator;
 public class CustomComparator implements Comparator<ServiceRecord> {
 	@Override
 	public int compare(ServiceRecord r1, ServiceRecord r2) {
-		return r1.getDate().compareTo(r2.getDate());
+		String[] word1 = new String[3];
+		String[] word2 = new String[3];
+		word1= r1.getDate().split("/");
+		word2 = r2.getDate().split("/");
+
+		if (word1[1].compareTo(word2[2]) == 0) {
+			if (word1[1].compareTo(word2[1]) == 0) {
+				return  0 - word1[0].compareTo(word2[0]);
+			} else {
+				return 0 - word1[1].compareTo(word2[1]);
+			}
+		} else {
+			return 0- word1[2].compareTo(word2[2]);
+		}
 	}
 }
